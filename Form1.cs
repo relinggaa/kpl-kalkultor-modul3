fd27b5237864d99208e10ba3bc529d2abca6441c
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-05 09:08:57 +0700

    Menambahkan gui layout

--- Form1.cs
+++ Form1.cs
@@ -11,7 +11,7 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WindowsFormsApp1
 {
-
+   //Relingga Aditya
     public partial class Form1: Form
     {
         string angka1;
